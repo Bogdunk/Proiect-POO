b20d2427536cb122d1dc10b24c6fdbb1b963ba14
Bogdunk <63792626+Bogdunk@users.noreply.github.com>
2020-12-14 19:53:06 +0200

    Update child.cpp

--- child.cpp
+++ child.cpp
@@ -5,13 +5,24 @@
 child::child(){
     child_name = "copil";
     age = 0;
+    a_fost_cuminte = 1;
 }
 
-void child::get_new_child(string name, int ages) {
+void child::get_new_child(string name, int ages, bool cuminte) {
     child_name = name;
     age = ages;
+    a_fost_cuminte = cuminte;
 }
 
 void child::show_child_and_toy( toy& jucarie ) {
     cout << child_name << " are o/un " << jucarie.nume << " din " << jucarie.material << " pentru copii de peste " << jucarie.age_limit << " ani.\n\n";
 }
+
+int child::get_a_fost_cuminte() {
+   return a_fost_cuminte;
+}
+
+void child::get_name() {
+    cout << child_name;
+}
+
