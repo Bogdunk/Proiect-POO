7f686fd0601a85c08351488b9243d5d15209e684
Bogdunk <63792626+Bogdunk@users.noreply.github.com>
2020-12-14 19:55:24 +0200

    Update and rename sacul_lui_Mos_Craciun.cpp to Mos_Craciun.cpp

---
+++ Mos_Craciun.cpp
@@ -0,0 +1,51 @@
+#include "Mos_Craciun.h"
+#include "toy.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+/// stiu ca am in STL <stack>
+/// am vrut sa implementez eu o stiva, ai mult pentru exercitiu
+
+void Mos_Craciun::init(){
+    vf = 0;
+    cout << "Spuneti cate jucarii sa aduca Mos Craciun->\n";
+    cin >> SIZE;
+}
+
+void Mos_Craciun::push( string str ) {
+    if(vf == SIZE ) {
+        cout << "\nSacul lui Mosu este plin.\n";
+    }
+    else{
+        stck.push_back(str);
+        vf++;
+    }
+}
+
+void Mos_Craciun::pop()
+{
+    if(vf==0) {
+        cout << "Sacul este gol. Copilul are nevoie de jucarii.\n";
+    }
+    else{
+        vf--;
+        stck.pop_back();
+    }
+}
+
+void Mos_Craciun::show() {
+
+    if( vf == 0 )
+        cout << "Mosu nu are nici o jucarie in sac!\n";
+    else{
+        cout << "Mos Craciun aduce daruri la fetite si baieti: ";
+        if ( vf > 0 ){
+            int contor = vf - 1;
+            while ( contor >= 0 ){
+                cout << stck[ contor ] << " ";
+                contor--;
+            }
+        }
+    }
+}
