65e20f11613284299a7bc4162531aec4d460ccd3
Bogdunk <63792626+Bogdunk@users.noreply.github.com>
2021-01-12 09:29:48 +0200

    Update Mos_Craciun.h

--- Mos_Craciun.h
+++ Mos_Craciun.h
@@ -12,9 +12,27 @@ class Mos_Craciun{
     int vf;
     int SIZE;
 public:
+    Mos_Craciun();
     void init();
-    void push(string str);
+    Mos_Craciun( const Mos_Craciun& );
+    void push( string str );
     void pop();
     void show();
+    /*friend ostream& oprerator<<( ostream& out , const Mos_Craciun& mos ){
+
+        if ( mos.vf == 0 )
+            out << "Sacul este gol!\n";
+        else{
+            if( mos.vf > 0 ){
+                int contor = mos.vf - 1;
+                while (contor >= 0 ){
+                    out << mos.stck[ contor ] << ", ";
+                    contor--;
+                }
+            }
+            out << "\n";
+        }
+        return out;
+    }*/
 };
 #endif //CLASE_SACUL_LUI_MOS_CRACIUN_H
